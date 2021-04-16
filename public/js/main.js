c5fe1adfa70f9a144a3988a62cf7d787b75ce276
Hakael666 <66447825+Hakael666@users.noreply.github.com>
2021-04-15 20:40:56 -0500

    Update main.js

--- public/js/main.js
+++ public/js/main.js
@@ -82,7 +82,7 @@ function outputUsers(users) {
 
 //Prompt the user before leave chat room
 document.getElementById('leave-btn').addEventListener('click', () => {
-  const leaveRoom = confirm('estas seguro de salir de la sala?');
+  const leaveRoom = confirm('Are you sure you want to leave the chatroom?');
   if (leaveRoom) {
     window.location = '../index.html';
   } else {
